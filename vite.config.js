39c2dda94dd22c59b7026d58d04f4df9a3225394
Rahul-Dhage <rahuldhage2804@yahoo.com>
2024-12-31 01:14:30 +0530

    vite-fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Expose to all network interfaces
+    port: 6969,
+  },
+  preview: {
+    host: true, // Expose to all network interfaces
+    port: 6969,
+  },
+});
